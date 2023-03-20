74c3f643091b068902e5319e38d1c7696a49fdf8
i2xv <iscoramadane@gmail.com>
2023-03-21 00:07:33 +0100

    first commit

---
+++ next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    appDir: true,
+  },
+};
+
+module.exports = nextConfig;
+
+module.exports = {
+  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
